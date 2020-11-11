d209930f461ceb1a13513db73095de505a02b6f3
indi94 <58439092+indi94@users.noreply.github.com>
2020-11-11 23:48:05 +0100

    Add files via upload
    GUIT

---
+++ app.js
@@ -0,0 +1,30 @@
+// Init github
+const github = new Github;
+const ui = new UI;
+
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup',(e) => {
+// Get Input text
+const userText = e.target.value;
+
+if(userText !==''){
+    github.getUser(userText)
+    .then(data => {
+        if(data.profile.message === 'Not Found'){
+            // Show alert
+            ui.showAlert('User not found', 'alert alert-danger');
+        } else {
+            // Show profile
+            ui.showProfile(data.profile);
+            ui.showRepos(data.repos);
+        }
+    })
+   
+} else {
+    // Clear profile
+    ui.clearProfile();
+}
+});
